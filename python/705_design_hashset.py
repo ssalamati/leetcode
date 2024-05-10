1336dcdc49a41ca88df2f624117616253233bc24
Sami Salamati <sami@d142-058-088-253.wireless.sfu.ca>
2024-05-10 14:12:05 -0700

    Create: 705_design_hashset.py

---
+++ python/705_design_hashset.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class MyHashSet:
+
+    def __init__(self):
+        self.set = [Node(0) for _ in range(10**4)]
+
+    def add(self, key: int) -> None:
+        index = key % len(self.set)
+        cur = self.set[index]
+        while cur.next:
+            if cur.next.val == key:
+                return
+
+            cur = cur.next
+
+        cur.next = Node(key)
+
+    def remove(self, key: int) -> None:
+        index = key % len(self.set)
+        cur = self.set[index]
+        while cur.next:
+            if cur.next.val == key:
+                cur.next = cur.next.next
+                return
+
+            cur = cur.next
+
+    def contains(self, key: int) -> bool:
+        index = key % len(self.set)
+        cur = self.set[index]
+        while cur.next:
+            if cur.next.val == key:
+                return True
+
+            cur = cur.next
+
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
